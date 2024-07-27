2600e19b8ce457205d4e1e37bf2470f43838a065
bombin78 <bombin78@mail.ru>
2024-07-27 20:09:24 +0600

    add new rule: public-api-imports

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -1,24 +1,17 @@
-/**
- * @fileoverview feature sliced relative path checker
- * @author alexander
- */
 "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-const rule = require("../../../lib/rules/path-checker"),
-  RuleTester = require("eslint").RuleTester;
-
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+const rule = require("../../../lib/rules/path-checker");
+const RuleTester = require("eslint").RuleTester;
 
 const ruleTester = new RuleTester({
   parserOptions: {ecmaVersion: 6, sourceType: 'module'}
 });
+
+const aliasOptions = [
+  {
+    alias: '@'
+  }
+];
 
 ruleTester.run("path-checker", rule, {
   valid: [
@@ -33,11 +26,7 @@ ruleTester.run("path-checker", rule, {
       filename: '/home/bombin/dev/react18-app/src/entities/Article',
       code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
       errors: [{ message: "В рамках одного слайса все пути должны быть относительными"}],
-      options: [
-        {
-          alias: '@'
-        }
-      ]
+      options: aliasOptions,
     },
     {
       filename: '/home/bombin/dev/react18-app/src/entities/Article',
